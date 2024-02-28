9310166a612207aa191fbeba348db6b69717aeec
gnkz <gnkz.dev@gmail.com>
2024-02-27 23:05:22 -0300

    feat: add gen.nvim plugin and configure it to use the "deepseek-coder" model, replace copilot.vim with copilot.lua

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,13 @@
 return {
-	"github/copilot.vim"
+	"zbirenbaum/copilot.lua",
+	config = function()
+		require("copilot").setup({
+			suggestion = {
+				auto_trigger = true,
+				keymap = {
+					accept = "<C-l>",
+				}
+			}
+		})
+	end
 }
