359324fb86fff296b6e365d9ee8a189812eebc24
gnkz <gnkz.dev@gmail.com>
2024-02-28 13:13:04 -0300

    chore: update configuration for copilot and cmp plugins, and update key mappings for lspsaga and trouble plugins

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,13 +1,17 @@
 return {
-	"zbirenbaum/copilot.lua",
-	config = function()
-		require("copilot").setup({
-			suggestion = {
-				auto_trigger = true,
-				keymap = {
-					accept = "<C-l>",
-				}
-			}
-		})
-	end
+	{
+		"zbirenbaum/copilot.lua",
+		config = function()
+			require("copilot").setup({
+				suggestion = { enabled = false },
+				panel = { enabled = false },
+			})
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
 }
