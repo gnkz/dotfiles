67bfe2c287a4a246c9f4bfcded05a5fb81a31885
Gonzalo <gnkz@users.noreply.github.com>
2024-02-28 02:01:11 -0300

    feat: update configuration for nvim-tree plugin to hide dotfiles and change keymap for toggling file finder (#11)

--- nvim/.config/nvim/lua/plugins/explorer.lua
+++ nvim/.config/nvim/lua/plugins/explorer.lua
@@ -1,7 +1,11 @@
 return {
 	"nvim-tree/nvim-tree.lua",
 	config = function()
-		require("nvim-tree").setup()
-		vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
-	end
+		require("nvim-tree").setup({
+			filters = {
+				dotfiles = false,
+			},
+		})
+		vim.keymap.set("n", "<leader>e", ":NvimTreeFindFileToggle<CR>", { silent = true })
+	end,
 }
