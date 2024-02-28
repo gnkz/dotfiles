72596557671cb0953ab3564d0b6ca6bb89f78cc7
Gonzalo <gnkz@users.noreply.github.com>
2024-02-28 01:56:07 -0300

    chore: update neovim configuration for formatting and LSP servers (#10)

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -11,24 +11,24 @@ return {
 			keys = {
 				{ "<leader>rn", "<cmd>Lspsaga rename<cr>" },
 				{ "<leader>ca", "<cmd>Lspsaga code_action<CR>" },
-				{ "<leader>o",  "<cmd>Lspsaga outline<cr>" },
-				{ "gf",         "<cmd>Lspsaga lsp_finder<cr>" },
-				{ "gD",         "<cmd>Lspsaga peek_definition<cr>" },
-				{ "gd",         "<cmd>Lspsaga goto_definition<CR>" },
-				{ "gt",         "<cmd>Lspsaga goto_type_definition<CR>" },
-				{ "gT",         "<cmd>Lspsaga peek_type_definition<CR>" },
+				{ "<leader>o", "<cmd>Lspsaga outline<cr>" },
+				{ "gf", "<cmd>Lspsaga lsp_finder<cr>" },
+				{ "gD", "<cmd>Lspsaga peek_definition<cr>" },
+				{ "gd", "<cmd>Lspsaga goto_definition<CR>" },
+				{ "gt", "<cmd>Lspsaga goto_type_definition<CR>" },
+				{ "gT", "<cmd>Lspsaga peek_type_definition<CR>" },
 				{ "<leader>sh", "<cmd>Lspsaga hover_doc<cr>" },
-				{ "[d",         "<cmd>Lspsaga diagnostic_jump_prev<cr>" },
-				{ "]d",         "<cmd>Lspsaga diagnostic_jump_next<cr>" },
+				{ "[d", "<cmd>Lspsaga diagnostic_jump_prev<cr>" },
+				{ "]d", "<cmd>Lspsaga diagnostic_jump_next<cr>" },
 				{ "<leader>dl", "<cmd>Lspsaga show_line_diagnostics<cr>" },
 			},
 			config = function()
 				require("lspsaga").setup({
 					symbol_in_winbar = {
-						enable = false
-					}
+						enable = false,
+					},
 				})
-			end
+			end,
 		},
 		{
 			"folke/trouble.nvim",
@@ -42,7 +42,7 @@ return {
 			},
 			config = function()
 				require("trouble").setup({})
-			end
+			end,
 		},
 	},
 	config = function()
@@ -67,7 +67,7 @@ return {
 			"rust_analyzer",
 			"solidity_ls_nomicfoundation",
 			"terraformls",
-			"gopls"
+			"gopls",
 		}
 
 		masonconfig.setup({
@@ -75,14 +75,15 @@ return {
 		})
 
 		require("neodev").setup({})
+
 		local rust_format_augroup = vim.api.nvim_create_augroup("RustFormatting", {})
 		require("rust-tools").setup({
 			tools = {
 				autoSetHints = true,
 				inlay_hints = {
 					auto = true,
-					show_parameter_hints = true
-				}
+					show_parameter_hints = true,
+				},
 			},
 			server = {
 				on_attach = function(client, bufnr)
@@ -99,21 +100,21 @@ return {
 					["rust-analyzer"] = {
 						checkOnSave = {
 							enable = true,
-							command = "clippy"
+							command = "clippy",
 						},
 						cargo = {
-							allFeatures = true
+							allFeatures = true,
 						},
 						format = { enable = false },
-					}
-				}
-			}
+					},
+				},
+			},
 		})
 
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 		lspconfig.util.default_config = vim.tbl_deep_extend("force", lspconfig.util.default_config, {
-			capabilities = capabilities
+			capabilities = capabilities,
 		})
 
 		lspconfig.dockerls.setup({})
@@ -126,6 +127,9 @@ return {
 					runtime = {
 						version = "LuaJIT",
 					},
+					workspace = {
+						library = vim.api.nvim_get_runtime_file("", true),
+					},
 					completion = {
 						callSnippet = "Replace",
 					},
@@ -133,33 +137,20 @@ return {
 						enable = true,
 					},
 					format = {
-						enable = true,
-						defaultConfig = {
-							quote_style = "double"
-						}
-					}
-				}
-			}
+						enable = false,
+					},
+				},
+			},
 		})
 
-		lspconfig.eslint.setup({
+		lspconfig.tsserver.setup({
 			on_attach = function(client, bufnr)
 				client.server_capabilities.documentFormattingProvider = false
 			end,
 		})
-
-
-		local tsserver_format_augroup = vim.api.nvim_create_augroup("TsServerFormatting", {})
-		lspconfig.tsserver.setup({
+		lspconfig.eslint.setup({
 			on_attach = function(client, bufnr)
-				vim.api.nvim_clear_autocmds({ group = tsserver_format_augroup, buffer = bufnr })
 				client.server_capabilities.documentFormattingProvider = false
-				vim.api.nvim_create_autocmd("BufWritePre", {
-					group = tsserver_format_augroup,
-					callback = function()
-						vim.cmd("Neoformat prettierd")
-					end
-				})
 			end,
 		})
 
@@ -171,13 +162,11 @@ return {
 				vim.api.nvim_create_autocmd("BufWritePre", {
 					group = zig_format_augroup,
 					callback = function()
-						vim.lsp.buf.format({
-							bufnr = bufnr
-						})
+						vim.lsp.buf.format()
 					end,
 				})
 			end,
-			cmd = { "zls" }
+			cmd = { "zls" },
 		})
 
 		local solidity_format_augroup = vim.api.nvim_create_augroup("SolidityFormat", {})
@@ -185,27 +174,27 @@ return {
 		lspconfig.solidity_ls_nomicfoundation.setup({
 			on_attach = function(client, bufnr)
 				vim.api.nvim_clear_autocmds({ group = solidity_format_augroup, buffer = bufnr })
-				client.server_capabilities.documentFormattingProvider = false;
+				client.server_capabilities.documentFormattingProvider = false
 				vim.api.nvim_create_autocmd("BufWritePre", {
 					pattern = { "*.sol" },
 					group = solidity_format_augroup,
 					callback = function()
 						vim.cmd("Neoformat solidity forge")
-					end
+					end,
 				})
-			end
+			end,
 		})
 
-		lspconfig.terraformls.setup {
+		lspconfig.terraformls.setup({
 			on_attach = function(client, bufnr)
 				vim.api.nvim_create_autocmd("BufWritePre", {
 					pattern = { "*.tf", "*.tfvars" },
 					callback = function()
 						vim.lsp.buf.format()
-					end
+					end,
 				})
-			end
-		}
+			end,
+		})
 
 		lspconfig.gopls.setup({
 			settings = {
@@ -225,9 +214,9 @@ return {
 					pattern = { "*.sql" },
 					callback = function()
 						vim.cmd("Neoformat sleek")
-					end
+					end,
 				})
-			end
+			end,
 		})
-	end
+	end,
 }
