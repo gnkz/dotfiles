936cc2ace96b7c56f12dd6243c14d7aac407618b
Gonzalo <gnkz@users.noreply.github.com>
2024-02-28 13:12:47 -0300

    feat: add Zen Mode plugin configuration for Neovim with keybinding, options, and callbacks (#14)

---
+++ nvim/.config/nvim/lua/plugins/zen.lua
@@ -0,0 +1,17 @@
+return {
+	"folke/zen-mode.nvim",
+	keys = {
+		{ "<leader>z", "<cmd>ZenMode<cr>" },
+	},
+	opts = {
+		plugins = {
+			tmux = { enable = true },
+		},
+		on_open = function()
+			vim.cmd("set laststatus=0")
+		end,
+		on_close = function()
+			vim.cmd("set laststatus=3")
+		end,
+	},
+}
