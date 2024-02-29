df9a72e0b0866cccc4720dfd584efa44a9e5da05
Gonzalo <gnkz@users.noreply.github.com>
2024-02-29 16:23:37 -0300

    feat: enable auto-trigger for copilot suggestions and update keymap for accepting suggestions in copilot.lua configuration (#25)

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -58,7 +58,6 @@ return {
 					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
-					{ name = "copilot", group_index = 2 },
 					{ name = "nvim_lsp", group_index = 2 },
 					{ name = "vsnip", group_index = 2 },
 				}, {
